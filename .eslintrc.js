c18118dcb3a5c5298f1b921d3b7873383b2f3562
Robinson <robinson.bonnard@gmail.com>
2018-07-10 09:19:51 +0100

    👕

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [ 'airbnb' ],
+
+  plugins: [ 'json' ],
+
+  env: { browser: true },
+
+  parserOptions: { ecmaVersion: 8 },
+
+  rules: {
+    'max-len': [
+      "error",
+      140,
+      {
+        ignoreTrailingComments: true,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'linebreak-style': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ]
+  }
+}
